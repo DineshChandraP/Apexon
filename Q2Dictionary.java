4cff98ba3fec9bb01d1a449d2fc22358ce21a5d8
DineshChandra <pdc.dinesh@gmail.com>
2024-10-18 10:25:02 +0530

    Initial Commit

---
+++ Q2Dictionary.java
@@ -0,0 +1,24 @@
+package com.GS;
+
+public class Q2Dictionary {
+
+	public static void main(String[] args) {
+		String[] dictionary = { "CODGE", "LODGESSSS", "LODGES", "123", "dodge", "mODJ", "LODGESSS" };
+		String toSearch = "ODG";
+
+		String result = searchAndReturnMaxLengthWord(dictionary, toSearch);
+		System.out.println(result);
+	}
+
+	private static String searchAndReturnMaxLengthWord(String[] dictionary, String toSearch) {
+		int maxLength = 0;
+		int maxElementIndex = 0;
+		for (int i = 0; i < dictionary.length; i++) {
+			if (dictionary[i].contains(toSearch) && dictionary[i].length() > maxLength) {
+				maxLength = dictionary[i].length();
+				maxElementIndex = i;
+			}
+		}
+		return dictionary[maxElementIndex];
+	}
+}

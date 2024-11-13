3773de6eb663db6f885b1f66bdb16f63d3d0201b
DineshChandra <pdc.dinesh@gmail.com>
2024-11-13 15:13:08 +0530

    Adding few solutions

---
+++ Reference Areas Practice/Q14NumberPowerOfTen.java
@@ -0,0 +1,32 @@
+
+public class Q14NumberPowerOfTen {
+
+	public static void main(String[] args) {
+
+		int n = 1000;
+
+		checkIftheGivenNumberIsPowerOfTenV1(n);
+		checkIftheGivenNumberIsPowerOfTenV2(n);
+
+	}
+
+	private static void checkIftheGivenNumberIsPowerOfTenV1(int n) {
+
+		if (n <= 0) {
+			System.out.println("Invalid Number");
+		}
+
+		System.out.println((int) Math.log10(n) == (int) Math.log10(n));
+	}
+
+	private static void checkIftheGivenNumberIsPowerOfTenV2(int n) {
+		if (n <= 0) {
+			System.out.println("Invalid Number");
+		}
+
+		while (n % 10 == 0) {
+			n = n / 10;
+		}
+		System.out.println(n == 1);
+	}
+}

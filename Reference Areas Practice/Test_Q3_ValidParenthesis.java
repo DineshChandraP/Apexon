18095505ec3024ba4adce453c208fa9b60e79a58
DineshChandra <pdc.dinesh@gmail.com>
2024-11-20 16:19:05 +0530

    Adding solutions

--- Reference Areas Practice/Test_Q3_ValidParenthesis.java
+++ Reference Areas Practice/Test_Q3_ValidParenthesis.java
@@ -5,59 +5,53 @@ public class Test_Q3_ValidParenthesis {
 
 	public static void main(String[] args) {
 
-		String expression = "y + (a + (b -c) +z - z)";
+		String str = "(a + (b -c) +z) - z)";//extra closing
+		String str1 = "(a + (b -c) +z - z)";//valid
+		String str2 = "((a + (b -c) +z - z)";//extra opening
+		String str3 = "(a + {b -c)  +z}";//bracket types do not match/ brackets overlapping
+		String str4 = "y + (a + (b -c) +z - z) ";//valid
 
-		if (checkValidParenthesis(expression)) {
+		if (checkValidParenthesis(str4)) {
 			System.out.println("Given expression is valid");
 		} else {
 			System.out.println("Given expression is not valid");
 		}
 	}
 
-	private static boolean checkValidParenthesis(String expression) {
-		//To push the opening chars
+	private static boolean checkValidParenthesis(String str) {
+		// To push the opening chars
 		Stack<Character> stack = new Stack<Character>();
-
-		//Pairs
+		// Pairs
 		HashMap<Character, Character> map = new HashMap<Character, Character>();
 		map.put('}', '{');
 		map.put(')', '(');
 		map.put(']', '[');
 
-		for (char c : expression.toCharArray()) {
-
+		for (char c : str.toCharArray()) {
 			// If character is opening ([{, push it to stack
 			if (map.containsValue(c)) {
 				stack.push(c);
 			}
-			
-			
 			// If character is closing )]}
 			if (map.containsKey(c)) {
-
 				if (!stack.isEmpty()) {
-					
-					//check if the closing char matches with the opening char
+					// check if the closing char matches with the opening char
 					if (stack.peek() == map.get(c)) {
 						stack.pop();
 					} else {
 						return false;
 					}
-					
 				} else {
-					//Closing character found, but Stack is empty-opening char is missing
+					// Closing character found, but Stack is empty-opening char is missing
 					return false;
 				}
 			}
 		}
-
 		// If stack is empty, return true, else false
 		if (!stack.isEmpty())
 			return false;
-		
 		// No conflicts, valid parenthesis
 		return true;
-		
-		//Time Complexity: O(n)- number of chars in String.
+		// Time Complexity: O(n)- number of chars in String.
 	}
 }

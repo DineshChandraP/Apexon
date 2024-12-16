280641feba501b907641dc4daf7ae516c8ee88ca
DineshChandra <pdc.dinesh@gmail.com>
2024-12-16 13:13:49 +0530

    Linux Practice

--- Reference Areas Practice/java8/StreamsPractice.java
+++ Reference Areas Practice/java8/StreamsPractice.java
@@ -2,7 +2,10 @@ package java8;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+
 import java8.Transaction.Type;
 
 public class StreamsPractice {
@@ -14,6 +17,29 @@ public class StreamsPractice {
 	        transactions.add(new Transaction(3, Type.GROCERY, new BigDecimal("50.25")));
 	        transactions.add(new Transaction(4, Type.GROCERY, new BigDecimal("500.00")));
 	        
-	        
+	        // Filter and sort transactions
+	        List<Transaction> groceryTransactions = new ArrayList<>();
+	        for (Transaction t : transactions) {
+	            if (t.getType() == Type.GROCERY) {
+	                groceryTransactions.add(t);
+	            }
+	        }
+
+	        // Sort by value in descending order
+	        Collections.sort(groceryTransactions, new Comparator<Transaction>() {
+	            @Override
+	            public int compare(Transaction t1, Transaction t2) {
+	                return t2.getAmount().compareTo(t1.getAmount());
+	            }
+	        });
+
+	        // Collect the transaction IDs after sorting
+	        List<Integer> transactionIds = new ArrayList<>();
+	        for (Transaction t : groceryTransactions) {
+	            transactionIds.add(t.getId());
+	        }
+
+	        // Print sorted transaction IDs
+	        System.out.println("Sorted transaction IDs by value: " + transactionIds);
+	    }
 	}
-}

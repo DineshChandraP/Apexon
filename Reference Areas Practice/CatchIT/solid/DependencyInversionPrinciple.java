4402ca21250936bf688bad93475fb0dafb676353
DineshChandra <pdc.dinesh@gmail.com>
2025-01-22 14:30:46 +0530

    Adding Solutions

--- Reference Areas Practice/CatchIT/solid/DependencyInversionPrinciple.java
+++ Reference Areas Practice/CatchIT/solid/DependencyInversionPrinciple.java
@@ -1,6 +1,54 @@
 package CatchIT.solid;
 
 public class DependencyInversionPrinciple {
+	
+	
+	class UserService {
+	    private Database database;
+
+	    public UserService() {
+	        database = new Database(); // Tight coupling to Database class
+	    }
+
+	    public void saveUser(User user) {
+	        database.save(user);
+	    }
+	}
+
+	class Database {
+	    public void save(User user) {
+	        // Save user to the database
+	    }
+	}
+	
+	
+	interface DataSource {
+	    void save(User user);
+	}
+
+	class Database implements DataSource {
+	    public void save(User user) {
+	        // Save user to database
+	    }
+	}
+
+	class FileDataSource implements DataSource {
+	    public void save(User user) {
+	        // Save user to a file
+	    }
+	}
+
+	class UserService {
+	    private DataSource dataSource;
+
+	    public UserService(DataSource dataSource) {
+	        this.dataSource = dataSource;
+	    }
+
+	    public void saveUser(User user) {
+	        dataSource.save(user);
+	    }
+	}
 	class UserService {
 	    private Database database;
 
--- Reference Areas Practice/CatchIT/solid/OpenClosedPrinciple.java
+++ Reference Areas Practice/CatchIT/solid/OpenClosedPrinciple.java
@@ -1,6 +1,46 @@
 package CatchIT.solid;
 
 public class OpenClosedPrinciple {
+	
+	class TaxCalculator {
+	    public double calculateTax(String country, double amount) {
+	        if (country.equals("USA")) {
+	            return amount * 0.10;  // USA tax rate
+	        } else if (country.equals("Canada")) {
+	            return amount * 0.15;  // Canada tax rate
+	        }
+	        return 0;
+	    }
+	}
+	
+	interface TaxStrategy {
+	    double calculateTax(double amount);
+	}
+
+	class USATaxStrategy implements TaxStrategy {
+	    public double calculateTax(double amount) {
+	        return amount * 0.10;  // USA tax rate
+	    }
+	}
+
+	class CanadaTaxStrategy implements TaxStrategy {
+	    public double calculateTax(double amount) {
+	        return amount * 0.15;  // Canada tax rate
+	    }
+	}
+
+	class TaxCalculator {
+	    private TaxStrategy taxStrategy;
+
+	    public TaxCalculator(TaxStrategy taxStrategy) {
+	        this.taxStrategy = taxStrategy;
+	    }
+
+	    public double calculateTax(double amount) {
+	        return taxStrategy.calculateTax(amount);
+	    }
+	}
+	
 	class DiscountCalculator {
 	    public double calculateDiscount(String customerType, double price) {
 	        if (customerType.equals("Regular")) {
